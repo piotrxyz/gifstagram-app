5bafec057e76307784dd5cf756cbbc0612c29b92
piotrxyz <piotr.zalom@gmail.com>
2020-04-29 19:23:46 +0200

    Updates

---
+++ precache-manifest.f6e855c1497120716b0f2ddfaffae866.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d009554585dd864874a22735e5ffe8f",
+    "url": "/mkcoders-app/index.html"
+  },
+  {
+    "revision": "cf1d43fa7b7dd5f551ec",
+    "url": "/mkcoders-app/static/css/main.b5f319fb.chunk.css"
+  },
+  {
+    "revision": "11a5a30e2eb8ed4e2dd9",
+    "url": "/mkcoders-app/static/js/2.210654f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mkcoders-app/static/js/2.210654f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf1d43fa7b7dd5f551ec",
+    "url": "/mkcoders-app/static/js/main.0a6299e5.chunk.js"
+  },
+  {
+    "revision": "7142848d842273bc82f1",
+    "url": "/mkcoders-app/static/js/runtime-main.0f98ce45.js"
+  },
+  {
+    "revision": "802cee8f0bfcc4c61ab25fb025224fcc",
+    "url": "/mkcoders-app/static/media/column.802cee8f.svg"
+  },
+  {
+    "revision": "278d8d8468abf147dbb87bba5b70f757",
+    "url": "/mkcoders-app/static/media/search.278d8d84.svg"
+  },
+  {
+    "revision": "ca8e9547866cf10900099d71b5042304",
+    "url": "/mkcoders-app/static/media/threecolumns.ca8e9547.svg"
+  },
+  {
+    "revision": "b313c33c241f1a2cfa6f91422f3f232b",
+    "url": "/mkcoders-app/static/media/twocolumns.b313c33c.svg"
+  }
+]);
